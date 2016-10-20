bcb1598a66d129d190f6f88542464d056c60bd69
Xujie <six@gatech.edu>
2016-10-20 00:20:29 -0400

    add EDB facts/constraints in context

--- src/context.cpp
+++ src/context.cpp
@@ -1,11 +1,25 @@
 
 #include "context.h"
+#include "extern.h"
 
 #include <algorithm>
 #include <map>
 
 namespace SpeedyFOIL {
 
+namespace {
+
+int estimateBits(int sz) {
+	int K = 1;
+	while (sz) {
+		++K;
+		sz >>= 1;
+	}
+
+	return K;
+}
+
+} // end of anonymous namespace
 
 void ContextManager::buildSorts(TypeInfo* TArr, int N) {
 	const int START = 1; // to confirm
@@ -14,22 +28,17 @@ void ContextManager::buildSorts(TypeInfo* TArr, int N) {
 	for(int i=START; i < END; ++i) {
 		TypeInfo t = TArr[i];
 
-		int sz  = t->NValues;
-
-		int K = 1;
-		while(sz) {
-			++K;
-			sz >>= 1;
-		}
-
+		int K = estimateBits(t->NValues);
 		z3::sort s = C.bv_sort( K );
 
 		sortMap.insert( std::make_pair(t, s) );
 	}
+
+	MaxBits = estimateBits(MaxConst);
 }
 
 void ContextManager::buildFuncDecls(Relation* RArr, int N) {
-	const int START = 1; // to confirm
+	const int START = 0; // to confirm
 	const int END = N; // to confirm
 
 	for(int i=START; i < END; ++i) {
@@ -44,6 +53,7 @@ void ContextManager::buildFuncDecls(Relation* RArr, int N) {
 		z3::sort_vector sv(C);
 
 		for(int j=1; j <= rel->Arity; ++j){
+			/*
 			TypeInfo t = rel->TypeRef[j];
 			auto it = sortMap.find(t);
 			if(it == sortMap.end()) {
@@ -51,11 +61,65 @@ void ContextManager::buildFuncDecls(Relation* RArr, int N) {
 			}
 
 			sv.push_back( it->second );
+			*/
+
+			sv.push_back( C.bv_sort(MaxBits) );
 		}
 
 		z3::func_decl pred = C.function(rel->Name, sv, C.bool_sort());
 
 		funcMap.insert( std::make_pair(rel, pred) );
+	}
+}
+
+
+void ContextManager::loadEDBFacts(Relation* RArr, int N) {
+	for(int i=0; i<N; ++i) {
+		Relation rel = RArr[i];
+		if(rel->Name[0] == '=' || rel->Name[0] == '>') {
+			continue;
+		}
+
+		std::set<std::vector<unsigned int>> st;
+
+
+		if(!(rel->PossibleTarget)) {
+			Tuple* tp = rel->Pos;
+			while(*tp){
+
+				std::vector<unsigned int> v;
+				for(int j=1; j <= rel->Arity; ++j){
+					v.push_back( (unsigned int) *tp[j]);
+				}
+
+				st.insert(std::move(v));
+
+				++tp;
+			}
+		}
+
+		EDBFacts.insert( std::make_pair(rel, std::move(st)) );
+	}
+}
+
+
+void ContextManager::appendEDBConstr(Z3_fixedpoint& fp) {
+	for (auto pr : EDBFacts) {
+		Relation rel = pr.first;
+
+		auto it = funcMap.find(rel);
+		if (it == funcMap.end()) {
+			std::cerr << "ERROR, buildEDBFacts got unknown relation : "
+					<< rel->Name << std::endl;
+		}
+
+		z3::func_decl f = it->second;
+
+		const std::set<std::vector<unsigned int> >& st = pr.second;
+
+		for (auto v : st) {
+			Z3_fixedpoint_add_fact(C, fp, f, v.size(), v.data());
+		}
 	}
 }
 
--- src/context.h
+++ src/context.h
@@ -12,15 +12,27 @@
 
 namespace SpeedyFOIL {
 
+
+struct EDBValue {
+
+};
+
+
 struct ContextManager{
 	z3::context C;
 
+	int MaxBits;
+
 	std::map<TypeInfo, z3::sort> sortMap;
+
 	std::map<Relation, z3::func_decl> funcMap;
+	std::map<Relation, std::set<std::vector<unsigned int>>> EDBFacts;
 
 	void buildSorts(TypeInfo* TArr, int N);
 	void buildFuncDecls(Relation* RArr, int N);
+	void loadEDBFacts(Relation* RArr, int N);
 
+	void appendEDBConstr(Z3_fixedpoint& fp) ;
 };
 
 
